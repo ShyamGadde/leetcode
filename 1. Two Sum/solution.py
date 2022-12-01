74fdd290a344ca1f9161b37153c6e49ab7e13936
Shyamsundar Gadde <shyamgadde1602@gmail.com>
2022-12-01 19:36:42 +0530

    Add 1. Two Sum

---
+++ 1. Two Sum/solution.py
@@ -0,0 +1,16 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        prev_elems = {}
+
+        for i, n in enumerate(nums):
+            idx = prev_elems.get(target - n)
+            if idx is not None:
+                return [idx, i]
+            prev_elems[n] = i
+
+
+"""
+Why not first create the hash table/map first and then search?
+>> It will consider the same element twice if the value of `n` is `target / 2`. To solve that we will need to check the 
+indices of the values.  
+"""
