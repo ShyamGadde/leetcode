c2ea8b1fff94fce088932fbb790905894ffcce4e
Shyamsundar Gadde <shyamgadde1602@gmail.com>
2022-12-05 17:45:19 +0530

    Add alternate solution for 125. Valid Palindrome

--- 125. Valid Palindrome/solution.py
+++ 125. Valid Palindrome/solution.py
@@ -6,3 +6,28 @@ class Solution:
 # Time complexity: O(N)
 # Space complexity: O(N) (Technically the same variable 's' is used so many be not but maybe for s[::-1])
 
+
+# Alternative solution
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        l, r = 0, len(s) - 1
+        s = s.lower()
+
+        while l < r:
+            while l < r and not self.alnum(s[l]):
+                l += 1
+            while r > l and not self.alnum(s[r]):
+                r -= 1
+            if s[l] != s[r]:
+                return False
+            l += 1
+            r -= 1
+        return True
+
+    def alnum(self, c):
+        return (65 <= ord(c) <= 90 or
+                97 <= ord(c) <= 122 or
+                48 <= ord(c) <= 57)
+
+# Time complexity; O(N)
+# Space complexity: O(1)
