843d6ec832c5c052d1bd331b606c64ed4b88431d
Shyamsundar Gadde <shyamgadde1602@gmail.com>
2022-12-06 20:54:48 +0530

    Add 1. Two Sum

--- 1. Two Sum/solution.py
+++ 1. Two Sum/solution.py
@@ -1,3 +1,15 @@
+# @before-stub-for-debug-begin
+from python3problem1 import *
+from typing import *
+# @before-stub-for-debug-end
+
+#
+# @lc app=leetcode id=1 lang=python3
+#
+# [1] Two Sum
+#
+
+# @lc code=start
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         prev_elems = {}
@@ -7,7 +19,8 @@ class Solution:
             if diff in prev_elems:
                 return [prev_elems[diff], i]
             prev_elems[n] = i
-
+        
+# @lc code=end
 
 """
 Why not first create the hash table/map first and then search?
