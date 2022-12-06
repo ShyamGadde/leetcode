7dea5e7e03bd29c5c17da707bd7d6ca0e6e6ba49
Shyamsundar Gadde <shyamgadde1602@gmail.com>
2022-12-06 18:50:15 +0530

    Add Iterative solution for 226. Invert Binary Tree

--- 226. Invert Binary Tree/solution.py
+++ 226. Invert Binary Tree/solution.py
@@ -4,12 +4,37 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+
+# Recursive Solution
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
         if not root:
             return None
 
         root.right, root.left = self.invertTree(root.left), self.invertTree(root.right)
+
+        return root
+
+# Time complexity: O(N)
+# Space complexity: O(N)
+
+
+# Iterative Solution
+class Solution:
+    def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        if not root:
+            return None
+
+        queue = collections.deque([root])
+        while queue:
+            current = queue.popleft()
+            current.left, current.right = current.right, current.left
+
+            if current.left:
+                queue.append(current.left)
+
+            if current.right:
+                queue.append(current.right)
 
         return root
 
