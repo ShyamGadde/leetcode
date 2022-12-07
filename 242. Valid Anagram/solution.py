f4698d9abf7a69adeb8a2f22fdb722a7818bd8c2
Shyamsundar Gadde <shyamgadde1602@gmail.com>
2022-12-07 05:30:05 +0530

    Add Alternative solution for 242. Valid Anagram

--- 242. Valid Anagram/solution.py
+++ 242. Valid Anagram/solution.py
@@ -9,6 +9,19 @@ from collections import *
 # @lc code=start
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
-        return Counter(s) == Counter(t)
+        pass
 # @lc code=end
 
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return Counter(s) == Counter(t)
+
+# Time complexity: O(S + T)
+# Space complexity: O(S + T)
+
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        return sorted(s) == sorted(t)
+
+# Time complexity: O(NLogN)
+# Space complexity: O(1) (Technically!)
