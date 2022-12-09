8500671202f677af82537a669b3fbc980aeb2850
Shyamsundar Gadde <shyamgadde1602@gmail.com>
2022-12-09 12:09:10 +0530

    Remove solutions for problems not in Blind 75

--- 1704. Determine if String Halves Are Alike/solution1.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def halvesAreAlike(self, s: str) -> bool:
-        d = len(s) // 2
-        return sum(ch.lower() in "aeiou" for ch in s[:d]) == sum(ch.lower() in "aeiou" for ch in s[d:])
-
-# Time complexity: O(N^2)
-# Space complexity: O(1)
