29eccdcd293f7734ce83e871b0588665170da0d2
Shyamsundar Gadde <shyamgadde1602@gmail.com>
2022-12-11 17:44:05 +0530

    Solve 104. Maximum Depth of Binary Tree using Iterative DFS (preorder traversal)

--- 104. Maximum Depth of Binary Tree/solution.py
+++ 104. Maximum Depth of Binary Tree/solution.py
@@ -13,21 +13,16 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
-        if not root:
-            return 0
-        
-        from collections import deque
-        levels = 0
-        queue = deque([root])
-        while queue:
-            for _ in range(len(queue)):
-                node = queue.popleft()
-                if node.left:   
-                    queue.append(node.left)
-                if node.right:
-                    queue.append(node.right)
-            levels += 1
-        return levels
+        stack = [(root, 1)]
+        res = 0
+
+        while stack:
+            node, depth = stack.pop()
+            if node:
+                res = max(res, depth)
+                stack.append((node.left, depth + 1))
+                stack.append((node.right, depth + 1))
+        return res
 
 # Time complexity: O(N)
 # Space complexity: O(N)
@@ -41,5 +36,25 @@ class Solution:
 # Time complexity: O(N)
 # Space complexity: O(N)
 
+# Using BFS
+# class Solution:
+#     def maxDepth(self, root: Optional[TreeNode]) -> int:
+#         if not root:
+#             return 0
+        
+#         from collections import deque
+#         levels = 0
+#         queue = deque([root])
+#         while queue:
+#             for _ in range(len(queue)):
+#                 node = queue.popleft()
+#                 if node.left:   
+#                     queue.append(node.left)
+#                 if node.right:
+#                     queue.append(node.right)
+#             levels += 1
+#         return levels
 
+# Time complexity: O(N)
+# Space complexity: O(N)
 
