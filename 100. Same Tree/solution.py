3acc43018111ad93f7a01045ffa91220c1a108af
Shyamsundar Gadde <shyamgadde1602@gmail.com>
2022-12-12 06:02:36 +0530

    Solve 100. Same Tree using Recursive DFS

--- 100. Same Tree/solution.py
+++ 100. Same Tree/solution.py
@@ -16,35 +16,36 @@ class Solution:
         if not p and not q:
             return True
 
-        if not p or not q:
+        if not p or not q or p.val != q.val:
             return False
 
-        queue1 = deque([p])
-        queue2 = deque([q])
+        return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
 
-        while queue1 and queue2:
-            node1 = queue1.popleft()
-            node2 = queue2.popleft()
-
-            if node1 is None and node2 is None:
-                continue
-
-            if not node1 or not node2:
-                return False
-
-            if node1.val != node2.val:
-                return False
-
-            queue1.append(node1.left)
-            queue1.append(node1.right)
-
-            queue2.append(node2.left)
-            queue2.append(node2.right)
-
-        return queue1 == queue2
-
-# Time complexity: O(P + Q)
-# Space complexity: O(P + Q)
+# Time complexity: O(N)
+# Space complexity: O(N)
         
 # @lc code=end
 
+# Using BFS
+# class Solution:
+#     def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+#         while queue1:
+#             node1 = queue1.popleft()
+#             node2 = queue2.popleft()
+
+#             if not node1 and not node2:
+#                continue
+
+#            if not node1 or not node2 or node1.val != node2.val:
+#                return False
+
+#             queue1.append(node1.left)
+#             queue1.append(node1.right)
+
+#             queue2.append(node2.left)
+#             queue2.append(node2.right)
+
+#         return True
+
+# Time complexity: O(N)
+# Space complexity: O(N)
