1284496df3a6e57f1ef2e2292849058c1f048b7d
ShyamGadde <73636812+ShyamGadde@users.noreply.github.com>
2023-05-01 15:33:34 +0530

    Add 252. Meeting Rooms

---
+++ 252. Meeting Rooms/solution.py
@@ -0,0 +1,20 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    """
+    @param intervals: an array of meeting time intervals
+    @return: if a person could attend all meetings
+    """
+    def can_attend_meetings(self, intervals: List[Interval]) -> bool:
+        intervals.sort(key=lambda x:x.start)
+
+        return all(
+            intervals[i - 1].end <= intervals[i].start
+            for i in range(1, len(intervals))
+        )
