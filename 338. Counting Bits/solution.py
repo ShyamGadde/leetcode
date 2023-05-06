bed209e7c907ae8826a285ebca6898d92610909d
ShyamGadde <73636812+ShyamGadde@users.noreply.github.com>
2023-05-06 11:07:01 +0530

    Add 338. Counting Bits

---
+++ 338. Counting Bits/solution.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=338 lang=python3
+#
+# [338] Counting Bits
+#
+
+
+# @lc code=start
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        res = [0] * (n + 1)
+        for i in range(1, n + 1):
+            res[i] = res[i >> 1] + (i & 1)
+        return res
+# @lc code=end
