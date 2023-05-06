d5b093863f1727afc88066464e7b4c5157e76c94
ShyamGadde <73636812+ShyamGadde@users.noreply.github.com>
2023-05-06 11:07:25 +0530

    Add 190. Reverse Bits

---
+++ 190. Reverse Bits/solution.py
@@ -0,0 +1,23 @@
+# @before-stub-for-debug-begin
+# from python3problem190 import *
+from typing import *
+# @before-stub-for-debug-end
+
+#
+# @lc app=leetcode id=190 lang=python3
+#
+# [190] Reverse Bits
+#
+
+# @lc code=start
+class Solution:
+    def reverseBits(self, n: int) -> int:
+        ans = 0
+        for _ in range(32):
+            ans = (ans << 1) | (n & 1)
+            n >>= 1
+        return ans
+# @lc code=end
+
+Solution().reverseBits(43261596)
+
