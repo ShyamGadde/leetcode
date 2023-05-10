d29340cadd9fd1fa0ae8e96310b2c06d52fc9254
ShyamGadde <73636812+ShyamGadde@users.noreply.github.com>
2023-05-10 18:31:17 +0530

    Add 268. Missing Number

---
+++ 268. Missing Number/solution.py
@@ -0,0 +1,15 @@
+#
+# @lc app=leetcode id=268 lang=python3
+#
+# [268] Missing Number
+#
+
+# @lc code=start
+class Solution:
+    def missingNumber(self, nums: List[int]) -> int:
+        missing_number = len(nums)
+        for i, num in enumerate(nums):
+            missing_number ^= i ^ num
+        return missing_number
+# @lc code=end
+
