bef73b8a92a0483006f652a070472d46bf12f0c7
ShyamGadde <73636812+ShyamGadde@users.noreply.github.com>
2023-05-13 17:12:58 +0530

    Add 1046. Last Stone Weight

---
+++ 1046. Last Stone Weight/solution.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode id=1046 lang=python3
+#
+# [1046] Last Stone Weight
+#
+
+# @lc code=start
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        # Python does not have a max heap, so we need to invert the sign
+        stones = [-s for s in stones]
+        heapq.heapify(stones)
+
+        while len(stones) > 1:
+            y = heapq.heappop(stones)
+            x = heapq.heappop(stones)
+            if x != y:
+                heapq.heappush(stones, y - x)
+        
+        return -stones[0] if stones else 0
+# @lc code=end
+
