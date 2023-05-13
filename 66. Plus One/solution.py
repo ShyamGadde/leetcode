e5899127ebb25138416dfe908248a1a892affbeb
ShyamGadde <73636812+ShyamGadde@users.noreply.github.com>
2023-05-13 17:45:19 +0530

    Add 66. Plus One

---
+++ 66. Plus One/solution.py
@@ -0,0 +1,17 @@
+#
+# @lc app=leetcode id=66 lang=python3
+#
+# [66] Plus One
+#
+
+# @lc code=start
+class Solution:
+    def plusOne(self, digits: List[int]) -> List[int]:
+        for i in range(len(digits)):
+            if digits[~i] < 9:
+                digits[~i] += 1
+                return digits
+            digits[~i] = 0
+        return [1] + [0] * len(digits)
+# @lc code=end
+
