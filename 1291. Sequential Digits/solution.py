66ca7d9293d5f940b58d64c6968a235706690dbb
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2024-02-02 11:52:30 +0530

    Add Python solution for 1291. Sequential Digits problem

---
+++ 1291. Sequential Digits/solution.py
@@ -0,0 +1,57 @@
+#
+# @lc app=leetcode id=1291 lang=python3
+#
+# [1291] Sequential Digits
+#
+
+# @lc code=start
+class Solution(object):
+    def sequentialDigits(self, low, high):
+        """
+        :type low: int
+        :type high: int
+        :rtype: List[int]
+        """
+        all_nums = [
+            12,
+            23,
+            34,
+            45,
+            56,
+            67,
+            78,
+            89,
+            123,
+            234,
+            345,
+            456,
+            567,
+            678,
+            789,
+            1234,
+            2345,
+            3456,
+            4567,
+            5678,
+            6789,
+            12345,
+            23456,
+            34567,
+            45678,
+            56789,
+            123456,
+            234567,
+            345678,
+            456789,
+            1234567,
+            2345678,
+            3456789,
+            12345678,
+            23456789,
+            123456789,
+        ]
+
+        return [num for num in all_nums if low <= num <= high]
+
+
+# @lc code=end
