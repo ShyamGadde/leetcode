8f63100a7dbf9bd62ab5ff350d4b904e85fc8988
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2024-02-02 12:24:43 +0530

    Refactor sequentialDigits method to use a deque for improved efficiency

--- 1291. Sequential Digits/solution.py
+++ 1291. Sequential Digits/solution.py
@@ -4,6 +4,9 @@
 # [1291] Sequential Digits
 #
 
+from collections import deque
+
+
 # @lc code=start
 class Solution(object):
     def sequentialDigits(self, low, high):
@@ -12,46 +15,17 @@ class Solution(object):
         :type high: int
         :rtype: List[int]
         """
-        all_nums = [
-            12,
-            23,
-            34,
-            45,
-            56,
-            67,
-            78,
-            89,
-            123,
-            234,
-            345,
-            456,
-            567,
-            678,
-            789,
-            1234,
-            2345,
-            3456,
-            4567,
-            5678,
-            6789,
-            12345,
-            23456,
-            34567,
-            45678,
-            56789,
-            123456,
-            234567,
-            345678,
-            456789,
-            1234567,
-            2345678,
-            3456789,
-            12345678,
-            23456789,
-            123456789,
-        ]
+        res = []
+        queue = deque(range(1, 10))
 
-        return [num for num in all_nums if low <= num <= high]
+        while queue:
+            num = queue.popleft()
+            if low <= num <= high:
+                res.append(num)
+            if (last_digit := num % 10) < 9:
+                queue.append(num * 10 + last_digit + 1)
+
+        return res
 
 
 # @lc code=end
