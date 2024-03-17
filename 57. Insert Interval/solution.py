c8c296200ba200c199c3748ce89ad62b2b95191a
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2024-03-17 14:46:57 +0530

    Add Python solution for 57. Insert Interval

---
+++ 57. Insert Interval/solution.py
@@ -0,0 +1,88 @@
+#
+# @lc app=leetcode id=57 lang=python3
+#
+# [57] Insert Interval
+#
+# https://leetcode.com/problems/insert-interval/description/
+#
+# algorithms
+# Medium (39.79%)
+# Likes:    9782
+# Dislikes: 742
+# Total Accepted:    997.7K
+# Total Submissions: 2.5M
+# Testcase Example:  '[[1,3],[6,9]]\n[2,5]'
+#
+# You are given an array of non-overlapping intervals intervals where
+# intervals[i] = [starti, endi] represent the start and the end of the i^th
+# interval and intervals is sorted in ascending order by starti. You are also
+# given an interval newInterval = [start, end] that represents the start and
+# end of another interval.
+#
+# Insert newInterval into intervals such that intervals is still sorted in
+# ascending order by starti and intervals still does not have any overlapping
+# intervals (merge overlapping intervals if necessary).
+#
+# Return intervals after the insertion.
+#
+# Note that you don't need to modify intervals in-place. You can make a new
+# array and return it.
+#
+#
+# Example 1:
+#
+#
+# Input: intervals = [[1,3],[6,9]], newInterval = [2,5]
+# Output: [[1,5],[6,9]]
+#
+#
+# Example 2:
+#
+#
+# Input: intervals = [[1,2],[3,5],[6,7],[8,10],[12,16]], newInterval = [4,8]
+# Output: [[1,2],[3,10],[12,16]]
+# Explanation: Because the new interval [4,8] overlaps with
+# [3,5],[6,7],[8,10].
+#
+#
+#
+# Constraints:
+#
+#
+# 0 <= intervals.length <= 10^4
+# intervals[i].length == 2
+# 0 <= starti <= endi <= 10^5
+# intervals is sorted by starti in ascending order.
+# newInterval.length == 2
+# 0 <= start <= end <= 10^5
+#
+#
+#
+from typing import List
+
+
+# @lc code=start
+class Solution:
+    def insert(
+        self, intervals: List[List[int]], newInterval: List[int]
+    ) -> List[List[int]]:
+        res = []
+
+        for i in range(len(intervals)):
+            if newInterval[1] < intervals[i][0]:
+                res.append(newInterval)
+                res.extend(intervals[i:])
+                return res
+            elif newInterval[0] > intervals[i][1]:
+                res.append(intervals[i])
+            else:
+                # Merge the overlapping intervals
+                newInterval[0] = min(newInterval[0], intervals[i][0])
+                newInterval[1] = max(newInterval[1], intervals[i][1])
+
+        # If the newInterval is the last interval
+        res.append(newInterval)
+        return res
+
+
+# @lc code=end
