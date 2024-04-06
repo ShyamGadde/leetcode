272ac830ffde5bb77a07da1243dde3f31fd7ada3
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2024-04-06 13:15:44 +0530

    Reorganize files and folders

--- 125. Valid Palindrome/solution-1.py
+++
@@ -1,7 +0,0 @@
-class Solution:
-    def isPalindrome(self, s: str) -> bool:
-        s = "".join([ch for ch in s.lower() if ch.isalnum()])
-        return s == s[::-1]
-
-# Time complexity: O(N)
-# Space complexity: O(N)
