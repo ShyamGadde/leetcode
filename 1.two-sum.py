557388783edf094a0814b93eb4b877423cbb8500
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2024-05-23 02:47:45 +0530

    Update Python solution for 1. Two Sum

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -1,15 +1,40 @@
-# @before-stub-for-debug-begin
-from python3problem1 import *
+import bisect
+import collections
+import copy
+import datetime
+import functools
+import heapq
+import io
+import itertools
+import json
+import math
+import operator
+import random
+import re
+import statistics
+import string
+import sys
+from bisect import *
+from builtins import *
+from collections import *
+from copy import *
+from datetime import *
+from functools import *
+from heapq import *
+from io import *
+from itertools import *
+from json import *
+from math import *
+from operator import *
+from random import *
+from re import *
+from statistics import *
+from string import *
+from sys import *
 from typing import *
-# @before-stub-for-debug-end
 
-#
-# @lc app=leetcode id=1 lang=python3
-#
-# [1] Two Sum
-#
 
-# @lc code=start
+# @leet start
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         prev_elems = {}
@@ -19,4 +44,6 @@ class Solution:
                 return [prev_elems[diff], i]
             prev_elems[n] = i
 
-# @lc code=end
+
+# @leet end
+
