dc5626eb4ead60dc1906f719565435d98fa27610
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2024-06-14 22:09:12 +0530

    Update 2540.minimum-common-value.py

--- 2540.minimum-common-value.py
+++ 2540.minimum-common-value.py
@@ -1,6 +1,40 @@
-from typing import List
+import bisect
+import collections
+import copy
+import datetime
+import functools
+import heapq
+import io
+import itertools
+import json
+import math
+import operator
+import random
+import re
+import statistics
+import string
+import sys
+from bisect import *
+from builtins import *
+from collections import *
+from copy import *
+from datetime import *
+from functools import *
+from heapq import *
+from io import *
+from itertools import *
+from json import *
+from math import *
+from operator import *
+from random import *
+from re import *
+from statistics import *
+from string import *
+from sys import *
+from typing import *
 
 
+# @leet start
 class Solution:
     def getCommon(self, nums1: List[int], nums2: List[int]) -> int:
         i = j = 0
@@ -12,4 +46,9 @@ class Solution:
                 i += 1
             else:
                 j += 1
+
         return -1
+
+
+# @leet end
+
