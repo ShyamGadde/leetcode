d0183faf8d5aafd2d4d595313b4781b93a8abb3e
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2024-06-27 20:17:38 +0530

    Add 1791.find-center-of-star-graph.py

---
+++ 1791.find-center-of-star-graph.py
@@ -0,0 +1,45 @@
+from string import *
+from re import *
+from datetime import *
+from collections import *
+from heapq import *
+from bisect import *
+from copy import *
+from math import *
+from random import *
+from statistics import *
+from itertools import *
+from functools import *
+from operator import *
+from io import *
+from sys import *
+from json import *
+from builtins import *
+import string
+import re
+import datetime
+import collections
+import heapq
+import bisect
+import copy
+import math
+import random
+import statistics
+import itertools
+import functools
+import operator
+import io
+import sys
+import json
+from typing import *
+
+
+# @leet start
+class Solution:
+    def findCenter(self, edges: List[List[int]]) -> int:
+        first_edge, second_edge = edges[0], edges[1]
+
+        return first_edge[0] if first_edge[0] in second_edge else first_edge[1]
+
+
+# @leet end
