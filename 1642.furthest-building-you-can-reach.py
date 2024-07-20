f8b59ec706fc1803a56d5ce864bd52a196f9198e
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2024-07-20 12:10:24 +0530

    Update 1642.furthest-building-you-can-reach.py

--- 1642.furthest-building-you-can-reach.py
+++ 1642.furthest-building-you-can-reach.py
@@ -1,12 +1,8 @@
-#
-# @lc app=leetcode id=1642 lang=python3
-#
-# [1642] Furthest Building You Can Reach
-#
+from typing import List
 import heapq
 
 
-# @lc code=start
+# @leet start
 class Solution:
     def furthestBuilding(self, heights: List[int], bricks: int, ladders: int) -> int:
         heap = []  # Max heap
@@ -22,12 +18,10 @@ class Solution:
             if bricks < 0:
                 if ladders == 0:
                     return i
-                # If we don't have enough bricks, use a ladder instead of bricks
-                # on the largest difference in height so far.
                 ladders -= 1
                 bricks -= heapq.heappop(heap)
 
         return len(heights) - 1
 
 
-# @lc code=end
+# @leet end
