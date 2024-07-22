d94f8a1759ba0ee3d697b5b24e953caf77d38134
Shyamsundar Gadde <73636812+ShyamGadde@users.noreply.github.com>
2024-07-22 09:35:42 +0530

    Add 2418.sort-the-people.py

---
+++ 2418.sort-the-people.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+# @leet start
+class Solution:
+    def sortPeople(self, names: List[str], heights: List[int]) -> List[str]:
+        return map(lambda x: x[1], sorted(zip(heights, names), reverse=True))
+
+
+# @leet end
+
