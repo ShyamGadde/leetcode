78de216d049b7748b490e794e1bc3dfea93e2fa3
Shyamsundar Gadde <shyamgadde1602@gmail.com>
2025-01-08 03:23:50 +0530

    Add 1408.string-matching-in-an-array.py

---
+++ 1408.string-matching-in-an-array.py
@@ -0,0 +1,19 @@
+from typing import List
+
+
+# @leet start
+class Solution:
+    def stringMatching(self, words: List[str]) -> List[str]:
+        res = []
+
+        for i in range(len(words)):
+            for j in range(len(words)):
+                if i != j and words[i] in words[j]:
+                    res.append(words[i])
+                    break
+
+        return res
+
+
+# @leet end
+
